2a7cdd4568d2bb00fa0eef2bfa57fe2b08e93b25
LJAMINE <aminelmgrmj@gmail.com>
2025-03-24 12:27:24 +0000

    js

---
+++ Semaine 5 Intégration/jour 4 js/main.js
@@ -0,0 +1,61 @@
+let submit = document.getElementById("submit");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let tbody = document.getElementById("tbody");
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (name.value === "" || email.value === "" || password.value === "") {
+    alert("empty fields");
+  } else {
+    let row = document.createElement("tr");
+    
+
+let ceil0=row.insertCell(0)
+ceil0.innerText=name.value
+
+let ceil1=row.insertCell(1)
+ceil1.innerText=email.value
+
+let ceil2=row.insertCell(2)
+ceil2.innerText=password.value
+
+
+let btndelete=document.createElement("button")
+btndelete.innerText="delete"
+let ceil3=row.insertCell(3)
+ceil3.appendChild(btndelete)
+
+let btnupdate=document.createElement("button")
+btnupdate.innerText="update"
+let ceil4=row.insertCell(4)
+ceil4.appendChild(btnupdate)
+
+tbody.appendChild(row)
+
+
+
+btndelete.addEventListener('click',() => {
+    tbody.removeChild(row)
+})
+
+
+
+btnupdate.addEventListener("click",() => {
+    
+   name.value=ceil0.innerText
+   email.value=ceil1.innerText
+   password.value=ceil2.innerText
+   console.log(ceil0)
+})
+
+
+
+
+name.value=""
+email.value=""
+password.value=""
+
+  }
+});
